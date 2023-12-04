e392a69fed6d01046931b8c80064429691e15d47
Chris <cris_7715@hotmail.com>
2023-12-03 19:06:27 -0500

    entity 1.2

--- src/main/java/ec/voto/api/domain/Lista_Votos.java
+++ src/main/java/ec/voto/api/domain/Lista_Votos.java
@@ -1,4 +1,17 @@
 package ec.voto.api.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "lista_votos")
 public class Lista_Votos {
 }
