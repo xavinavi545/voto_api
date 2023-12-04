3a0c0b08c86f2306897ac06b44134b339f010027
Chris <cris_7715@hotmail.com>
2023-12-03 19:02:44 -0500

    entity 1.1

---
+++ src/main/java/ec/voto/api/domain/Estudiante.java
@@ -0,0 +1,19 @@
+package ec.voto.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "estudiante")
+
+public class Estudiante {
+
+}
